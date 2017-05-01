ca415b80cabb47f48dc4450d4c1740d8cb4fd152
Alfredo Oltra <alfredo.ptcf@gmail.com>
2017-05-01 13:35:26 +0200

    Support for python 2 & 3.

--- training/plain_word_freqs.py
+++ training/plain_word_freqs.py
@@ -1,33 +1,49 @@
+"""
+Create the word frequencies file from a corpus composed of several files
+
+Example usage: python plain_word_freqs ./corpus/*.txt freqs.txt
+"""
 from __future__ import unicode_literals
 
+import sys
 import codecs
 import glob
+from multiprocessing import Pool
 from collections import Counter
 
 import plac
-from multiprocessing import Pool
 from tqdm import tqdm
 
-
 def count_words(fpath):
-    with codecs.open(fpath, encoding="utf8") as f:
-        words = f.read().split()
+    """
+    Count the words of each file
+    """
+    with codecs.open(fpath, encoding="utf8") as file:
+        words = file.read().split()
         counter = Counter(words)
     return counter
 
 
 def main(input_glob, out_loc, workers=4):
-    p = Pool(processes=workers)
-    counts = p.map(count_words, tqdm(list(glob.glob(input_glob))))
+    """
+    Main function
+    """
+    pool = Pool(processes=workers)
+    counts = pool.map(count_words, tqdm(list(glob.glob(input_glob))))
     df_counts = Counter()
     word_counts = Counter()
-    for wc in tqdm(counts):
-        df_counts.update(wc.keys())
-        word_counts.update(wc)
-    with codecs.open(out_loc, "w", encoding="utf8") as f:
-        for word, df in df_counts.iteritems():
-            f.write(u"{freq}\t{df}\t{word}\n".format(word=repr(word), df=df, freq=word_counts[word]))
+    for wcount in tqdm(counts):
+        df_counts.update(wcount.keys())
+        word_counts.update(wcount)
+    with codecs.open(out_loc, "w", encoding="utf8") as file:
+        if sys.version_info[0] == 2:
+            items = df_counts.iteritems()
+        else:
+            items = df_counts.items()
 
+        for word, docf in items:
+            file.write(u"{freq}\t{df}\t{word}\n".format(word=repr(word),
+                                                        df=docf, freq=word_counts[word]))
 
 if __name__ == "__main__":
     plac.call(main)
